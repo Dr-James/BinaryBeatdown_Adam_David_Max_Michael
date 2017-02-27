22c33c8ef191e126896def711a5897423324908f
mjdryer <mjdryer@bsu.edu>
2017-02-27 14:36:33 -0500

    Working gui title screen and start button which moves to battle screen.

--- src/edu/bsu/cs222/binarybeatdown/GUI.java
+++ src/edu/bsu/cs222/binarybeatdown/GUI.java
@@ -1,85 +1,236 @@
 package edu.bsu.cs222.binarybeatdown;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
 import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
 import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
 import javafx.scene.paint.Paint;
 import javafx.scene.shape.Rectangle;
-import javafx.scene.text.*;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
+import static javafx.scene.text.FontWeight.BOLD;
+
 public class GUI extends Application {
-
-
-
+    private Button startButton;
+    private Scene battleScene;
+    private Stage thestage;
+    private Button firstAttackButton, secondAttackButton, thirdAttackButton, fourthAttackButton;
     @Override
-    public void start(Stage primaryStage) throws Exception{
-        primaryStage.setTitle("Binary Beatdown");
+    public void start(Stage battleStage) {
 
-        Button firstAttackButton = new Button("Attack 1");
-        Button secondAttackButton = new Button("Attack 2");
-        Button thirdAttackButton = new Button("Attack 3");
-        Button fourthAttackButton = new Button("Attack 4");
 
-        Pane battleScreen = new Pane();
 
+        Pane menuPane =new Pane();
+        Pane battlePane = new Pane();
+        thestage=battleStage;
+        //can now use the stage in other methods
+
+        //make things to put on panes
+
+
+        firstAttackButton = new Button("Attack 1");
+        firstAttackButton.setStyle("-fx-border-color: #550000; -fx-border-width: 2px;");
         firstAttackButton.setLayoutX(30);
         firstAttackButton.setLayoutY(30);
+        firstAttackButton.setOnAction(this::attackButtonClicked);
 
+        secondAttackButton = new Button("Attack 2");
+        secondAttackButton.setStyle("-fx-border-color: #550000; -fx-border-width: 2px;");
         secondAttackButton.setLayoutX(30);
         secondAttackButton.setLayoutY(100);
+        secondAttackButton.setOnAction(this::attackButtonClicked);
 
+        thirdAttackButton = new Button("Attack 3");
+        thirdAttackButton.setStyle("-fx-border-color: #550000; -fx-border-width: 2px;");
         thirdAttackButton.setLayoutX(30);
         thirdAttackButton.setLayoutY(170);
+        thirdAttackButton.setOnAction(this::attackButtonClicked);
 
+        fourthAttackButton = new Button("Attack 4");
+        fourthAttackButton.setStyle("-fx-border-color: #550000; -fx-border-width: 2px;");
         fourthAttackButton.setLayoutX(30);
         fourthAttackButton.setLayoutY(240);
+        fourthAttackButton.setOnAction(this::attackButtonClicked);
 
         Rectangle attackOutline = new Rectangle(20, 20, 95, 280);
         attackOutline.setStroke(Paint.valueOf("Black"));
-        attackOutline.setFill(null);
+        attackOutline.setFill(Color.LIGHTGRAY);
         attackOutline.setStrokeWidth(2);
 
         Rectangle screenBorder = new Rectangle(5, 5, 790, 390);
         screenBorder.setStroke(Paint.valueOf("Black"));
         screenBorder.setFill(null);
         screenBorder.setStrokeWidth(5);
+
         Rectangle textOutput = new Rectangle(20, 310, 760, 70);
         textOutput.setStroke(Paint.valueOf("Black"));
-        textOutput.setFill(null);
+        textOutput.setFill(Color.LIGHTGRAY);
         textOutput.setStrokeWidth(4);
 
         Rectangle userStats = new Rectangle(125, 250, 250, 50);
         userStats.setStroke(Paint.valueOf("Black"));
-        userStats.setFill(null);
+        userStats.setFill(Color.LIGHTGRAY);
         userStats.setStrokeWidth(2);
 
         Rectangle enemyStats = new Rectangle(510, 250, 270, 50);
         enemyStats.setStroke(Paint.valueOf("Black"));
-        enemyStats.setFill(null);
+        enemyStats.setFill(Color.LIGHTGRAY);
         enemyStats.setStrokeWidth(2);
 
-        Rectangle battleBorder = new Rectangle(128, 20, 650, 220);
+        Rectangle battleBorder = new Rectangle(125, 20, 655, 220);
         battleBorder.setStroke(Paint.valueOf("Black"));
-        battleBorder.setFill(null);
+        battleBorder.setFill(Color.LIGHTGRAY);
         battleBorder.setStrokeWidth(4);
 
         String userName = "USER";
         String enemyName = "ENEMY";
 
-        Text userVersusEnemy = new Text(userName + " vs " + enemyName);
-        userVersusEnemy.setX(392);
+        Text userVersusEnemy = new Text("vs.");
+        userVersusEnemy.setX(430);
         userVersusEnemy.setY(280);
 
-        battleScreen.getChildren().addAll(battleBorder, attackOutline, screenBorder, textOutput, userStats, enemyStats, userVersusEnemy);
-        battleScreen.getChildren().addAll(firstAttackButton, secondAttackButton, thirdAttackButton, fourthAttackButton);
-        primaryStage.setScene(new Scene(battleScreen, 800, 400));
-        primaryStage.show();
+        Text userNamePlacement = new Text(userName);
+        userNamePlacement.setX(420);
+        userNamePlacement.setY(260);
 
+        Text enemyNamePlacement = new Text(enemyName);
+        enemyNamePlacement.setX(420);
+        enemyNamePlacement.setY(300);
+
+        Canvas battleCanvas = new Canvas(800, 400);
+
+        //GraphicsContext testText = battleCanvas.getGraphicsContext2D();
+        //testText.setFill(Paint.valueOf("Red"));
+        //testText.setStroke(Paint.valueOf("Black"));
+        //testText.setLineWidth(1);
+        //javafx.scene.text.Font battleFont = javafx.scene.text.Font.font("Times New Roman", BOLD, 40);
+        //testText.setFont(battleFont);
+        //testText.fillText("TESTING", 180, 180);
+        //testText.strokeText("TESTING", 180, 180);
+        //testText.setFill(Paint.valueOf("Transparent"));
+
+        battlePane.getChildren().addAll(battleBorder, attackOutline, screenBorder, textOutput, userStats, enemyStats, userVersusEnemy);
+        battlePane.getChildren().addAll(firstAttackButton, secondAttackButton, thirdAttackButton, fourthAttackButton);
+        //battlePane.getChildren().addAll(battleCanvas);
+        battlePane.setStyle("-fx-background-color: gray;");
+
+
+
+
+
+        // END OF BATTLE PANE CODE, NOW ON TO THE MENU PANE CODE
+
+
+        Rectangle MenuScreenBorder = new Rectangle(5, 5, 790, 390);
+        MenuScreenBorder.setStroke(Paint.valueOf("Black"));
+        MenuScreenBorder.setFill(null);
+        MenuScreenBorder.setStrokeWidth(5);
+
+        Rectangle innerScreenBorder = new Rectangle(12, 12, 776, 376);
+        innerScreenBorder.setStroke(Paint.valueOf("Black"));
+        innerScreenBorder.setFill(null);
+        innerScreenBorder.setStrokeWidth(1);
+
+        Canvas canvas = new Canvas(800, 400);
+
+        GraphicsContext background = canvas.getGraphicsContext2D();
+        background.setFill(Paint.valueOf("Transparent"));
+        background.setStroke(Paint.valueOf("Black"));
+        background.setLineWidth(1);
+        javafx.scene.text.Font backFont = javafx.scene.text.Font.font("Times New Roman", BOLD, 56 );
+        background.setFont(backFont);
+        background.fillText("B I N A R Y   B E A T D O W N", 20, 202);
+        background.strokeText("B I N A R Y   B E A T D O W N", 20, 202);
+
+        GraphicsContext title = canvas.getGraphicsContext2D();
+        title.setFill(Paint.valueOf("Red"));
+        title.setStroke(Paint.valueOf("Black"));
+        title.setLineWidth(1);
+        javafx.scene.text.Font titleFont = javafx.scene.text.Font.font("Times New Roman", BOLD, 40);
+        title.setFont(titleFont);
+        title.fillText("B I N A R Y   B E A T D O W N", 130, 200);
+        title.strokeText("B I N A R Y   B E A T D O W N", 130, 200);
+
+        GraphicsContext subtitle = canvas.getGraphicsContext2D();
+        subtitle.setFill(Paint.valueOf("Red"));
+        subtitle.setStroke(Paint.valueOf("Black"));
+        subtitle.setLineWidth(1);
+        javafx.scene.text.Font subtitleFont = javafx.scene.text.Font.font("Times New Roman", BOLD, 15);
+        subtitle.setFont(subtitleFont);
+        subtitle.fillText("YOU  CAN  BE  A  ZERO,  OR  YOU  CAN  BE  THE  ONE.", 200, 230);
+        subtitle.strokeText("YOU  CAN  BE  A  ZERO,  OR  YOU  CAN  BE  THE  ONE", 200, 230);
+
+        GraphicsContext copyright = canvas.getGraphicsContext2D();
+        copyright.setFill(Paint.valueOf("Transparent"));
+        copyright.setStroke(Paint.valueOf("Black"));
+        copyright.setLineWidth(1);
+        javafx.scene.text.Font copyrightFont = javafx.scene.text.Font.font("Times New Roman", 8);
+        copyright.setFont(copyrightFont);
+        subtitle.fillText("C R E A T E D   B Y   M . A . D . M .   G A M I N G   C O O R P O R A T I O N", 260, 380);
+        subtitle.strokeText("C R E A T E D   B Y   M . A . D . M .   G A M I N G   C O O R P O R A T I O N", 260, 380);
+
+        GraphicsContext start = canvas.getGraphicsContext2D();
+        start.setFill(Paint.valueOf("Red"));
+        start.setStroke(Paint.valueOf("Black"));
+        start.setLineWidth(1);
+        javafx.scene.text.Font startFont = javafx.scene.text.Font.font("Times New Roman", BOLD, 25);
+        start.setFont(startFont);
+        start.fillText("S T A R T", 330, 300);
+        start.strokeText("S T A R T", 330, 300);
+
+        startButton=new Button("                              ");
+        startButton.setOnAction(this::StartButtonClicked);
+        startButton.setLayoutX(300);
+        startButton.setStyle("-fx-background-color: transparent");
+        startButton.setLayoutY(280);
+
+        //make 2 Panes
+
+
+        //set background color of each Pane
+        menuPane.setStyle("-fx-background-color: gray;-fx-padding: 10px;");
+        battlePane.setStyle("-fx-background-color: gray;-fx-padding: 10px;");
+
+        //add everything to panes
+        menuPane.getChildren().addAll(canvas, startButton, MenuScreenBorder, innerScreenBorder);
+        battlePane.getChildren().addAll();
+        //make 2 scenes from 2 panes
+
+        Scene menuScene = new Scene(menuPane, 800, 400);
+        battleScene = new Scene(battlePane, 800, 400);
+
+
+
+        battleStage.setTitle("B I N A R Y   B E A T D O W N");
+        battleStage.setScene(menuScene);
+        battleStage.show();
+    }
+
+    private void attackButtonClicked(ActionEvent event) {
+        if (event.getSource()==firstAttackButton)
+            System.out.println("It worked!");
+        if (event.getSource()==secondAttackButton)
+            System.out.println("It worked!");
+        if (event.getSource()==thirdAttackButton)
+            System.out.println("It worked!");
+        if (event.getSource()==fourthAttackButton)
+            System.out.println("It worked!");
 
 
     }
 
+    private void StartButtonClicked(ActionEvent e)
+    {
+        if (e.getSource()==startButton)
+            thestage.setScene(battleScene);
+    }
+
 
 }
+
+
